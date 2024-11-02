8e5507abe9ef4b57459236c8122374aa7d0a29c8
estebencalcina <kintopest@gmail.com>
2024-11-02 08:49:37 -0500

    Update start2.sh

--- start2.sh
+++ start2.sh
@@ -19,22 +19,22 @@ file_path='facefusion/uis/layouts/default.py'
 
 if [ -f "$file_path" ]; then
     echo "Modificando el archivo $file_path"
-    sed -i "s/ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'))/ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'), share=True)/" "$file_path"
+    sed -i "s/ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'))/ui.launch(favicon_path = 'facefusion.ico', inbrowser = state_manager.get_item('open_browser'), share=False)/" "$file_path"
 else
     echo "El archivo $file_path no existe."
     exit 1
 fi
 
-# Ejecución del script de facefusion
+# Ejecutar el script de facefusion en segundo plano
 python facefusion.py run &
 
-# Esperar antes de iniciar el túnel de Cloudflare
-sleep 120
+# Esperar un tiempo para que el servidor Gradio inicie
+sleep 20
 
-# Ejecutar el túnel de Cloudflare
-cloudflared tunnel --url localhost:7865
+# Iniciar el túnel de Cloudflare
+cloudflared tunnel --url localhost:7860 &
 
-# Manejo de argumentos para el script
+# Manejo de argumentos para el script de Cloudflare
 if [ $# -eq 0 ]; then
   sh cloudflare.sh
 elif [ $1 = "reset" ]; then
